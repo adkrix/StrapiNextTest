0fb1ea7d7583eb14102398ba65b3403509080a7d
Arthur Creek <arthur.creek@gmail.com>
2022-09-29 00:33:28 +0300

    useRef

--- ui/src/pages/register.tsx
+++ ui/src/pages/register.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import {useState, useRef, useEffect} from 'react';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
-const getDefaultElementError = () => ({
-  firstName: false,
-  email: false,
-  phone: false,
-  password: false,
-});
+const inputData = [
+  { name: 'firstName', type: 'text', title: 'First Name' },
+  { name: 'phone', type: 'tel', title: 'Phone number' },
+  { name: 'email', type: 'email', title: 'Email' },
+  { name: 'password', type: 'password', title: 'Password' },
+]
+const inputNames = inputData.map(({name}) => name);
+
+const getDefaultElementError = () => inputData.reduce((acc, data) => ({ ...acc, [data.name]: false }), {});
 
 const Register = () => {
   const [elementError, setElementError] = useState(getDefaultElementError());
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  let inputs = inputNames.reduce((acc, name) => ({ ...acc, [name]: useRef(null) }), {});
 
   const handleSubmit = (event: { preventDefault: () => void; target: HTMLInputElement; }) => {
     event.preventDefault();
@@ -21,8 +26,8 @@ const Register = () => {
     let data = {};
     let isFormError = false;
     let errors = {};
-    ['firstName', 'phone', 'email', 'password'].forEach((name: string) => {
-      const value = event.target.querySelector(`[name=${name}]`).value;
+    inputNames.forEach((name: string) => {
+      const value = inputs[name].current.value;
       if (!value) {
         isFormError = true;
         errors = {...errors, [name]: true };
@@ -49,7 +54,7 @@ const Register = () => {
         setSubmitted(true);
       }).finally(() => setLoading(false))
   }
-  console.log('elementError', elementError)
+  console.log('elementError', elementError, inputs);
   return (
     <Main
       meta={
@@ -63,22 +68,13 @@ const Register = () => {
         <h2 className="form__title">Register</h2>
           <form className={`form__area ${loading ? 'form__area--loading' : ''} ${submitted ? 'form__area--submitted' : ''}`}
               onSubmit={handleSubmit}>
-          <div className={`form__row ${elementError.firstName ? 'form__row--error' : ''}`}>
-            <label>First Name</label>
-            <input name="firstName" type="text" />
-          </div>
-          <div className={`form__row ${elementError.phone ? 'form__row--error' : ''}`}>
-            <label>Phone number</label>
-            <input name="phone" type="tel" />
-          </div>
-          <div className={`form__row ${elementError.email ? 'form__row--error' : ''}`}>
-            <label>Email</label>
-            <input name="email"  type="email" />
-          </div>
-          <div className={`form__row ${elementError.password ? 'form__row--error' : ''}`}>
-            <label>Password</label>
-            <input name="password" type="password" />
-          </div>
+            {inputData.map((({name, type, title}) => (
+              <div key={name} className={`form__row ${elementError[name] ? 'form__row--error' : ''}`}>
+                <label>{title}</label>
+                <input name={name} type={type} ref={inputs[name]} />
+              </div>
+            )))}
+
           <div className="form__button-row">
             <button className="form__button">Register</button>
           </div>
